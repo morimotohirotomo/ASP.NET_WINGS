98178ef4d035d4c20b8c5b8a2f05bf3d320d33c4
morimotohirotomo <minamoyori19@outlook.jp>
2020-10-07 17:52:37 +0900

    finish 08

---
+++ chapter02/WebApplication1/OutputCache.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class OutputCache : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = string.Format("時刻:{0}<br/>categoryキー:{1}<br/>keywordキー:{2}", DateTime.Now.ToLongTimeString(), Request.QueryString["category"], Request.QueryString["keyword"]);
+        }
+    }
+}
